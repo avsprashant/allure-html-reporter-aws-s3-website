14b0cecc35d9dca30443c953b1e92b926c22656d
Prashant <prashant.aradhyula@habitat.energy>
2023-09-11 16:49:51 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+#this isn't required if html file already present
           cat > index-template.html <<EOF
 
 <!DOCTYPE html>
@@ -30,19 +31,16 @@
 	<a href=".">.</a><br>
 
 EOF
-          
-unset JAVA_HOME
 
+export AWS_S3_BUCKET=habitat-build-artifacts
+export S3_WEBSITE_URL=https://artifact.habitatenergy.online/allure/${INPUT_GITHUB_REPO}/${INPUT_GITHUB_RUN_NUM}/
 
 mkdir -p ./${INPUT_ALLURE_HISTORY}
 
-if [[ ${INPUT_REPORT_URL} != '' ]]; then
-    S3_WEBSITE_URL="${INPUT_REPORT_URL}"
-fi
 #echo "executor.json"
 echo '{"name":"GitHub Actions","type":"github","reportName":"Allure Report with history",' > executor.json
-echo "\"url\":\"${GITHUB_PAGES_WEBSITE_URL}\"," >> executor.json # ???
-echo "\"reportUrl\":\"${GITHUB_PAGES_WEBSITE_URL}/${INPUT_GITHUB_RUN_NUM}/\"," >> executor.json
+echo "\"url\":\"${S3_WEBSITE_URL}\"," >> executor.json
+# echo "\"reportUrl\":\"${GITHUB_PAGES_WEBSITE_URL}/${INPUT_GITHUB_RUN_NUM}/\"," >> executor.json
 echo "\"buildUrl\":\"https://github.com/${INPUT_GITHUB_REPO}/actions/runs/${INPUT_GITHUB_RUN_ID}\"," >> executor.json
 echo "\"buildName\":\"GitHub Actions Run #${INPUT_GITHUB_RUN_ID}\",\"buildOrder\":\"${INPUT_GITHUB_RUN_NUM}\"}" >> executor.json
 #cat executor.json
@@ -59,7 +57,7 @@ ls -l ${INPUT_ALLURE_REPORT}
 
 echo "copy allure-report to ${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM}"
 cp -r ./${INPUT_ALLURE_REPORT}/. ./${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM}
-echo "copy allure-report history to /${INPUT_ALLURE_HISTORY}/last-history"
+# echo "copy allure-report history to /${INPUT_ALLURE_HISTORY}/last-history"
 # cp -r ./${INPUT_ALLURE_REPORT}/history/. ./${INPUT_ALLURE_HISTORY}/last-history
 
 # #echo "index.html"
@@ -70,12 +68,11 @@ echo "copy allure-report history to /${INPUT_ALLURE_HISTORY}/last-history"
 cat index-template.html > ./${INPUT_ALLURE_HISTORY}/index.html
 
 echo "├── <a href="./${INPUT_GITHUB_RUN_NUM}/index.html">Latest Test Results - RUN ID: ${INPUT_GITHUB_RUN_NUM}</a><br>" >> ./${INPUT_ALLURE_HISTORY}/index.html;
-sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 ls s3://${AWS_S3_BUCKET}" |  grep "PRE" | sed 's/PRE //' | sed 's/.$//' | sort -nr | while read line;
+sh -c "AWS_PROFILE=uk aws s3 ls s3://${AWS_S3_BUCKET}/allure/${INPUT_GITHUB_REPO}/" |  grep "PRE" | sed 's/PRE //' | sed 's/.$//' | sort -nr | while read line;
     do
         echo "├── <a href="./"${line}"/">RUN ID: "${line}"</a><br>" >> ./${INPUT_ALLURE_HISTORY}/index.html; 
     done;
 echo "</html>" >> ./${INPUT_ALLURE_HISTORY}/index.html;
-# cat ./${INPUT_ALLURE_HISTORY}/index.html
 
 
 echo "copy allure-results to ${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM}"
@@ -88,47 +85,20 @@ if [ -z "$AWS_S3_BUCKET" ]; then
   exit 1
 fi
 
-# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-#   exit 1
-# fi
-
-# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-#   exit 1
-# fi
-
-# Default to us-east-1 if AWS_REGION not set.
-# if [ -z "$AWS_REGION" ]; then
-#   AWS_REGION="us-east-1"
-# fi
-
+# we don't want this
 # Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
 if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
 fi
 
-# Create a dedicated profile for this action to avoid conflicts
-# with past/future actions.
-# https://github.com/jakejarvis/s3-sync-action/issues/1
-# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-# ${AWS_ACCESS_KEY_ID}
-# ${AWS_SECRET_ACCESS_KEY}
-# ${AWS_REGION}
-# text
-# EOF
-
-# Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --profile s3-sync-action \
+sh -c "AWS_PROFILE=uk aws s3 sync ./${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_RUN_NUM} s3://${AWS_S3_BUCKET}/allure/${INPUT_GITHUB_REPO}/${INPUT_GITHUB_RUN_NUM}/ \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
 
-
 # Delete history
-COUNT=$( sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 ls s3://${AWS_S3_BUCKET}" | sort -n | grep "PRE" | wc -l )
-echo "count folders in allure-history: ${COUNT}"
+COUNT=$( sh -c "AWS_PROFILE=uk aws s3 ls s3://${AWS_S3_BUCKET}/allure/${INPUT_GITHUB_REPO}/" | sort -n | grep "PRE" | wc -l )
+echo "current folders in allure-history: ${COUNT}"
 echo "keep reports count ${INPUT_KEEP_REPORTS}"
 INPUT_KEEP_REPORTS=$((INPUT_KEEP_REPORTS+1))
 echo "if ${COUNT} > ${INPUT_KEEP_REPORTS}"
@@ -136,20 +106,9 @@ if (( COUNT > INPUT_KEEP_REPORTS )); then
   NUMBER_OF_FOLDERS_TO_DELETE=$((${COUNT}-${INPUT_KEEP_REPORTS}))
   echo "remove old reports"
   echo "number of folders to delete ${NUMBER_OF_FOLDERS_TO_DELETE}"
-  sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 ls s3://${AWS_S3_BUCKET}" |  grep "PRE" | sed 's/PRE //' | sed 's/.$//' | head -n ${NUMBER_OF_FOLDERS_TO_DELETE} | sort -n | while read -r line;
+  sh -c "AWS_PROFILE=uk aws s3 ls s3://${AWS_S3_BUCKET}/allure/${INPUT_GITHUB_REPO}/" |  grep "PRE" | sed 's/PRE //' | sed 's/.$//' | head -n ${NUMBER_OF_FOLDERS_TO_DELETE} | sort -n | while read -r line;
     do
-      sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 rm s3://${AWS_S3_BUCKET}/${line}/ --recursive";
+      sh -c "AWS_PROFILE=uk aws s3 rm s3://${AWS_S3_BUCKET}/allure/${INPUT_GITHUB_REPO}/${line}/ --recursive";
       echo "deleted prefix folder : ${line}";
     done;
 fi
-
-# # Clear out credentials after we're done.
-# # We need to re-run `aws configure` with bogus input instead of
-# # deleting ~/.aws in case there are other credentials living there.
-# # https://forums.aws.amazon.com/thread.jspa?threadID=148833
-# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-# null
-# null
-# null
-# text
-# EOF
