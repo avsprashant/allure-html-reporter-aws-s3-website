81d2c628b093ce85c4be38bc3a9a6e2bfd9e46f1
Prashant <prashant.aradhyula@habitat.energy>
2023-09-11 13:23:29 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,9 +31,8 @@
 
 EOF
           
-          
-
 unset JAVA_HOME
+
 
 mkdir -p ./${INPUT_ALLURE_HISTORY}
 
@@ -71,7 +70,7 @@ echo "copy allure-report history to /${INPUT_ALLURE_HISTORY}/last-history"
 cat index-template.html > ./${INPUT_ALLURE_HISTORY}/index.html
 
 echo "├── <a href="./${INPUT_GITHUB_RUN_NUM}/index.html">Latest Test Results - RUN ID: ${INPUT_GITHUB_RUN_NUM}</a><br>" >> ./${INPUT_ALLURE_HISTORY}/index.html;
-sh -c "aws s3 ls s3://${AWS_S3_BUCKET}" |  grep "PRE" | sed 's/PRE //' | sed 's/.$//' | sort -nr | while read line;
+sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 ls s3://${AWS_S3_BUCKET}" |  grep "PRE" | sed 's/PRE //' | sed 's/.$//' | sort -nr | while read line;
     do
         echo "├── <a href="./"${line}"/">RUN ID: "${line}"</a><br>" >> ./${INPUT_ALLURE_HISTORY}/index.html; 
     done;
@@ -89,20 +88,20 @@ if [ -z "$AWS_S3_BUCKET" ]; then
   exit 1
 fi
 
-if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-  exit 1
-fi
+# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+#   exit 1
+# fi
 
-if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-  exit 1
-fi
+# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+#   exit 1
+# fi
 
 # Default to us-east-1 if AWS_REGION not set.
-if [ -z "$AWS_REGION" ]; then
-  AWS_REGION="us-east-1"
-fi
+# if [ -z "$AWS_REGION" ]; then
+#   AWS_REGION="us-east-1"
+# fi
 
 # Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
 if [ -n "$AWS_S3_ENDPOINT" ]; then
@@ -112,23 +111,23 @@ fi
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
+# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+# ${AWS_ACCESS_KEY_ID}
+# ${AWS_SECRET_ACCESS_KEY}
+# ${AWS_REGION}
+# text
+# EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
 
 
 # Delete history
-COUNT=$( sh -c "aws s3 ls s3://${AWS_S3_BUCKET}" | sort -n | grep "PRE" | wc -l )
+COUNT=$( sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 ls s3://${AWS_S3_BUCKET}" | sort -n | grep "PRE" | wc -l )
 echo "count folders in allure-history: ${COUNT}"
 echo "keep reports count ${INPUT_KEEP_REPORTS}"
 INPUT_KEEP_REPORTS=$((INPUT_KEEP_REPORTS+1))
@@ -137,20 +136,20 @@ if (( COUNT > INPUT_KEEP_REPORTS )); then
   NUMBER_OF_FOLDERS_TO_DELETE=$((${COUNT}-${INPUT_KEEP_REPORTS}))
   echo "remove old reports"
   echo "number of folders to delete ${NUMBER_OF_FOLDERS_TO_DELETE}"
-  sh -c "aws s3 ls s3://${AWS_S3_BUCKET}" |  grep "PRE" | sed 's/PRE //' | sed 's/.$//' | head -n ${NUMBER_OF_FOLDERS_TO_DELETE} | sort -n | while read -r line;
+  sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 ls s3://${AWS_S3_BUCKET}" |  grep "PRE" | sed 's/PRE //' | sed 's/.$//' | head -n ${NUMBER_OF_FOLDERS_TO_DELETE} | sort -n | while read -r line;
     do
-      sh -c "aws s3 rm s3://${AWS_S3_BUCKET}/${line}/ --recursive";
+      sh -c "AWS_PROFILE=${AWS_PROFILE} aws s3 rm s3://${AWS_S3_BUCKET}/${line}/ --recursive";
       echo "deleted prefix folder : ${line}";
     done;
 fi
 
-# Clear out credentials after we're done.
-# We need to re-run `aws configure` with bogus input instead of
-# deleting ~/.aws in case there are other credentials living there.
-# https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
+# # Clear out credentials after we're done.
+# # We need to re-run `aws configure` with bogus input instead of
+# # deleting ~/.aws in case there are other credentials living there.
+# # https://forums.aws.amazon.com/thread.jspa?threadID=148833
+# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+# null
+# null
+# null
+# text
+# EOF
